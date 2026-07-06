72182189cc8c9ff8a03b6bdc721f9401a2fcbe28
kla254505-svg <kla254505@gmail.com>
2026-07-06 23:40:01 +0700

    Update main.py

--- main.py
+++ main.py
@@ -96,7 +96,7 @@ def run_pipeline(df, symbol="SYMBOL", timeframe="15m", account_balance=1000.0, c
 if __name__ == "__main__":
     from fetch_data import fetch_twelvedata
 
-    symbols = [
+      symbols = [
         ("XAU/USD", "XAUUSD"),
     ]
 
